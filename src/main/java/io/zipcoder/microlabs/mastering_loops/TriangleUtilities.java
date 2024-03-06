c49802aa6af96c6e79b2550722244ad2934e89d3
Danny Cao <dannycao1997@yahoo.com>
2024-03-05 21:07:20 -0500

    triangles

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -2,19 +2,38 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TriangleUtilities {
 
-    public static String getTriangle(int numberOfRows) {
-        return null;
+    public static String getTriangle(int numberOfRows) { //getTriangleTest1 and getTriangleTest2
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < numberOfRows; i++) {
+            sb.append(getRow(i)).append("\n");
+        }
+        return sb.toString();
     }
 
+
     public static String getRow(int numberOfStars) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numberOfStars; i++) {
+            sb.append("*");
+        }
+        return sb.toString();
     }
 
     public static String getSmallTriangle() {
-        return getTriangle(5);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 4; i++) {
+            sb.append(getRow(i)).append("\n");
+        }
+        return sb.toString();
     }
 
+
     public static String getLargeTriangle() {
-        return getTriangle(15);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 9; i++) {
+            sb.append(getRow(i)).append("\n");
+        }
+        return sb.toString();
     }
 }
+
